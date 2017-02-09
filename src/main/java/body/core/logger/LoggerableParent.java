cad034f44f0949febb3ab0e0261d3cf99906a5d3
Victor Bolinches <vicboma1@users.noreply.github.com>
2017-02-09 18:32:44 +0100

    trait to logger messages

---
+++ src/main/java/body/core/logger/LoggerableParent.java
@@ -0,0 +1,13 @@
+package body.core.logger;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by vbolinch on 09/02/2017.
+ * Trait
+ * */
+public interface LoggerableParent {
+    default Logger get() {
+        return Logger.getLogger(this.getClass());
+    }
+}
